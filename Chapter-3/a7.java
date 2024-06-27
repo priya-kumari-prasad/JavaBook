add430e9b2a6395b1e44a28c9917bbaa1fba3365
Priya <aadhya.19185@gmail.com>
2024-06-27 18:08:26 +0000

    Jun 27, 2024, 11:38 PM

---
+++ Chapter-3/a7.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class calcSales {
+
+  public static void main(String[] args) {
+    float sales, Commission;
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter sales value: ");
+    sales = sc.nextFloat();
+    if (sales < 1000) {
+      System.out.println("Sales = " + sales + " So, There is no Commission.");
+    } else if (sales > 1000 && sales < 5000) {
+      Commission = sales * 0.1f;
+      System.out.println("Sales = " + sales + " \nSo the commission is " + Commission);
+
+    } else if (sales > 5000) {
+      Commission = 2000 + (0.8f * sales);
+      System.out.println("Sales = " + sales + "\n So the commision is " + Commission);
+    }
+
+  }
+}
