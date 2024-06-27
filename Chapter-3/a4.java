399d344d915a47d7419f3bca723d5290c4b104c9
Priya <aadhya.19185@gmail.com>
2024-06-27 17:14:07 +0000

    Jun 27, 2024, 10:44 PM

---
+++ Chapter-3/a4.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class senFaculty {
+  public static void main(String[] args) {
+    String name1, name2;
+    byte age1, age2;
+    Scanner inp = new Scanner(System.in);
+    System.out.println("Enter the name of both person : ");
+    name1 = inp.nextLine();
+    name2 = inp.nextLine();
+    System.out.println("Enter the age of both person: ");
+    age1 = inp.nextByte();
+    age2 = inp.nextByte();
+    if (age1 > age2) {
+      System.out.println(name1 + " is Senior faculty");
+    } else
+      System.out.println(name2 + " is Senior faculty.");
+    inp.close();
+  }
+}
