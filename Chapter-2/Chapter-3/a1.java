d561b9495e46b07d385c92f6eb6e0a30d648204e
Priya <aadhya.19185@gmail.com>
2024-06-27 16:45:12 +0000

    Jun 27, 2024, 10:15 PM

---
+++ Chapter-2/Chapter-3/a1.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Number {
+  public static void main(String[] args) {
+    int num;
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter a number");
+    num = sc.nextInt();
+    if (num == 0) {
+      System.out.println("Zero");
+    } else if (num > 0) {
+      System.out.println("Positive");
+    } else {
+      System.out.println("Negative");
+    }
+  }
+
+}
