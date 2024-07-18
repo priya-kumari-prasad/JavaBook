842de291e6c19f7e1e68c592c3e4ed0342ef82ee
Priya <aadhya.19185@gmail.com>
2024-07-18 22:46:31 +0530

    Chapter-5

--- Chapter-5/qu01.java
+++ Chapter-5/qu01.java
@@ -1,5 +1,59 @@
-package Chapter-5;
+import java.util.*;
 
-public class qu01 {
+class PrintWords {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    // System.out.println("Enter a Number: ");
+    // int num = sc.nextInt();
+    int num = 345;
+    int digit, reversed = 0;
+    while (num != 0) {
+      digit = num % 10;
+      reversed = reversed * 10 + digit;
+      num /= 10;
 
+    }
+    while (reversed > 0) {
+      digit = reversed % 10;
+      reversed /= 10;
+      switch (digit) {
+        case 1:
+          System.out.print("one ");
+          break;
+        case 2:
+          System.out.print("two ");
+          break;
+        case 3:
+          System.out.print("three ");
+          break;
+        case 4:
+          System.out.print("four ");
+          break;
+        case 5:
+          System.out.print("five");
+          break;
+        case 6:
+          System.out.print("six ");
+          break;
+        case 7:
+          System.out.print("seven ");
+          break;
+        case 8:
+          System.out.print("eight ");
+          break;
+        case 9:
+          System.out.print("nine ");
+          break;
+        case 0:
+          System.out.print("zero ");
+          break;
+        default:
+          System.out.println("Invalid input");
+          break;
+      }
+
+    }
+    System.out.println();
+
+  }
 }
