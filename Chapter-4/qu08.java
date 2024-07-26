1d9fe35f220efa08be3bc2065f4ff08a228fd215
Priya <aadhya.19185@gmail.com>
2024-07-26 17:03:06 +0000

    Jul 26, 2024, 10:33 PM

---
+++ Chapter-4/qu08.java
@@ -0,0 +1,10 @@
+class Numbers{
+  public static void main(String[] args) {
+    System.out.print("1 ");
+    System.out.print("2 ");
+    System.out.print("3 ");
+    System.out.print("4");
+    System.out.println();
+  }
+  
+}
