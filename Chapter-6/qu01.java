3b0e730f74a50c1d07fce2ae029ff36a4c50a371
Priya <aadhya.19185@gmail.com>
2024-07-29 11:40:45 +0000

    Jul 29, 2024, 5:10 PM

--- Chapter-6/qu01.java
+++ Chapter-6/qu01.java
@@ -2,7 +2,7 @@ class getChar {
   public static void main(String[] args) {
    char[] ch = {'A','a','d','h','y','a'};
    String name = new String(ch);
-    System.out.println(name);
+    System.out.println(name.charAt(3));
   }
   
 }
