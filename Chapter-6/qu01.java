18d1a03988943c36498be7eaefc46a0a1fa6e97c
Priya <aadhya.19185@gmail.com>
2024-07-31 11:04:43 +0000

    Jul 31, 2024, 4:34 PM

---
+++ Chapter-6/qu01.java
@@ -0,0 +1,11 @@
+/**
+ * qu01
+ */
+public class qu01 {
+
+  public static void main(String[] args) {
+    String s = "physics";
+    for(int j = 2;j<4;j++)
+    System.out.print(s.substring(j));
+  }
+}
