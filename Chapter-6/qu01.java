b099784c516a86e4658adf5f76d3dab4e2d98e2a
Priya <aadhya.19185@gmail.com>
2024-08-02 06:30:52 +0000

    Aug 2, 2024, 12:00 PM

---
+++ Chapter-6/qu01.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class getChar{
+  public static void main(String[] args) {
+    String str = "Aadhya Barnwal";
+    System.out.println("Enter Index value: ");
+    int idxVal = 5;
+    System.out.println(str.charAt(idxVal));    
+  }
+}
