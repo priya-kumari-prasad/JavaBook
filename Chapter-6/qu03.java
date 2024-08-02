6fc6a832b38803ed92cea2801ae7aa7dee16100c
Priya <aadhya.19185@gmail.com>
2024-08-02 06:47:12 +0000

    Aug 2, 2024, 12:17 PM

---
+++ Chapter-6/qu03.java
@@ -0,0 +1,13 @@
+class compare{
+  public static void main(String[] args) {
+    String str1 = "Aadhya";
+    String str2 = "Aadhya";
+    String str3 = "Sriad";
+
+
+    if(str1 == str3){
+      System.out.println(str1+" and "+str2+" are equal.");
+    }
+
+  }
+}
