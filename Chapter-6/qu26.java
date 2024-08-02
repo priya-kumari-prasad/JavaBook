7ce74dadc1ce8c7bb1c290e43b9ffd3ba3e20b5f
Priya <aadhya.19185@gmail.com>
2024-08-02 09:19:32 +0000

    Aug 2, 2024, 2:49 PM

--- Chapter-6/qu26.java
+++ Chapter-6/qu26.java
@@ -1,3 +1,6 @@
-class upper {
-  
+class upperCase {
+  public static void main(String[] args) {
+    String u = "UpperCase";
+    System.out.println(u.toUpperCase());
+  }  
 }
---
+++ Chapter-6/qu27.java
@@ -0,0 +1,7 @@
+class trim {
+  public static void main(String[] args) {
+    String t = "     Trailing WhiteSpace       ";
+    System.out.println(t.trim());
+  }
+  
+}
