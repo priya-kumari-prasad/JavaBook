d9571e4cb63e1da8ddecc210add4f02d4261be3a
Priya <aadhya.19185@gmail.com>
2024-08-02 06:53:56 +0000

    Aug 2, 2024, 12:23 PM

--- Chapter-6/qu03.java
+++ Chapter-6/qu03.java
@@ -2,11 +2,14 @@ class compare{
   public static void main(String[] args) {
     String str1 = "Aadhya";
     String str2 = "Aadhya";
-    String str3 = "Sriad";
+    String str3 = "john";
 
 
-    if(str1 == str3){
+    if(str1.equals(str2)){
       System.out.println(str1+" and "+str2+" are equal.");
+    }
+    if(!str1.equals(str3)){
+      System.out.println("not equal");
     }
 
   }
---
+++ Chapter-6/qu04.java
@@ -0,0 +1,10 @@
+
+class ignoreCase {
+  public static void main(String[] args) {
+    String str1 = "aaDHyA";
+    String str2 = "AAdhYa";
+
+    System.out.println(str1.equalsIgnoreCase(str2));
+  }
+  
+}
