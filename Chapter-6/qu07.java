f61fb1b5b90f9164fb1594771a8367d82aa4c4a7
Priya <aadhya.19185@gmail.com>
2024-08-03 07:56:48 +0000

    Aug 3, 2024, 1:26 PM

--- Chapter-6/qu07.java
+++ Chapter-6/qu07.java
@@ -1,6 +1,9 @@
 class compare {
   public static void main(String args[]){
-    
+    String str1 = "I can do anything.";
+    String str2 = "do";
+
+    System.out.println(str1.contains(str2));
   }
   
 }
