eddf4482b318a0f1f8c566171204d8674b64a9ea
Priya <aadhya.19185@gmail.com>
2024-08-03 08:59:12 +0000

    Aug 3, 2024, 2:29 PM

--- Chapter-6/qu08.java
+++ Chapter-6/qu08.java
@@ -2,16 +2,16 @@ class strBuffer {
   public static void main(String[] args) {
     String str = "Hey, How are You? ";
     StringBuffer sb = new StringBuffer("Hlo, I am perfectly fine.");
-    if(str.equals(sb)){
+    if (str.equals(sb)) {
       System.out.println("Both are equal.");
     }
-    
-    else if (str.contains(sb)){
+
+    else if (str.contains(sb)) {
       System.out.println("Str2 is available in Str1");
     }
-   
+
     else
-       System.out.println("You have given different string.");
-    
-  }  
+      System.out.println("You have given different string.");
+
+  }
 }
