513fff0d7d5caa6caa741bfff9195c3d2190af4d
Priya <aadhya.19185@gmail.com>
2024-08-03 09:03:10 +0000

    Aug 3, 2024, 2:33 PM

---
+++ Chapter-6/qu10.java
@@ -0,0 +1,7 @@
+class endsWithFun {
+  public static void main(String[] args) {
+    String str1 = "Hello , Mrs. mehra";
+    String str2 = "mehra";
+    System.out.println(str1.endsWith(str2));
+  }
+}
