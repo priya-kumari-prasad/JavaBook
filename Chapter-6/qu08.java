4a484c311a50e6ce4256a4dee83e6bee137ed121
Priya <aadhya.19185@gmail.com>
2024-08-03 08:56:18 +0000

    Aug 3, 2024, 2:26 PM

---
+++ Chapter-6/qu08.java
@@ -0,0 +1,8 @@
+class strBuffer {
+  public static void main(String[] args) {
+    String str = "Hey, How are You? ";
+    StringBuffer sb = new StringBuffer("Hlo, I am perfectly fine.");
+    System.out.println(sb.equals(str));
+    
+  }  
+}
