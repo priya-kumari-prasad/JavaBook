a400ed9adf49caf5081507d72495b7658a8b2f82
Priya <aadhya.19185@gmail.com>
2024-08-03 08:49:57 +0000

    Aug 3, 2024, 2:19 PM

--- Chapter-6/qu07.java
+++ Chapter-6/qu07.java
@@ -1,14 +1,22 @@
 class compare {
   public static void main(String args[]){
     String str1 = "I can do anything.";
-    String str2 = "do";
+    String str2 = "any";
 
-    System.out.println(str1.equals(str2));
-    System.out.println(str1.contains(str2));
-    System.out.println(str1.startsWith("I"));
-    System.out.println(str1.endsWith("g."));
-   
-
+    if(str1.equals(str2)){
+      System.out.println("Both are equal");
+    }
+    else if (str1.contains(str2)){
+      System.out.println("Str2 is available in Str1");
+    }
+    else if(str1.startsWith(str2)){
+      System.out.println("The str2 is starts with the str1");
+    }
+    else if(str1.endsWith(str2)){
+      System.out.println("The str2 is ends with str1.");
+    }
+    else
+       System.out.println("You have given different string.");
   }
 
   
