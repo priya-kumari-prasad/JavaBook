b388d188f08598c7b99f274d19f67aeeaeaf0c8c
Priya <aadhya.19185@gmail.com>
2024-08-04 10:04:12 +0000

    Aug 4, 2024, 3:34 PM

--- Chapter-6/qu22.java
+++ Chapter-6/qu22.java
@@ -1,7 +1,8 @@
 class replace {
   public static void main(String[] args) {
     String str = "ram is the friend of shyam, and he is also friend of mohan and all three are best friends.";
-   String replaceStr = str.replace("friend", "compainion");
+   String replaceStr = str.replace("friend", "companion");
+   System.out.println(replaceStr);
    System.out.println(replaceStr);
     
   }
