1b8e1787236c9fc64f905d2e5b4a9fb3948f8a11
Priya <aadhya.19185@gmail.com>
2024-08-04 10:10:35 +0000

    Aug 4, 2024, 3:40 PM

---
+++ Chapter-6/qu13.java
@@ -0,0 +1,8 @@
+import java.time.*;
+class date_time {
+  public static void main(String[] args) {
+  //  LocalDateTime = LocalDateTime.now();
+   System.out.println(LocalDateTime.now()); 
+  }
+  
+}
