77ec5c31421b282822b85975feb1cc19b206ee2c
Priya <aadhya.19185@gmail.com>
2024-08-04 15:47:06 +0000

    Aug 4, 2024, 9:17 PM

--- Chapter-6/qu13.java
+++ Chapter-6/qu13.java
@@ -1,8 +1,20 @@
 import java.time.*;
+import java.time.format.DateTimeFormatter;
 class date_time {
   public static void main(String[] args) {
-  //  LocalDateTime = LocalDateTime.now();
-   System.out.println(LocalDateTime.now()); 
+   LocalDateTime now = LocalDateTime.now();
+  //  System.out.println(LocalDateTime); 
+
+
+   //define the desired format
+   DateTimeFormatter formate = DateTimeFormatter.ofPattern("dd-mm-yyyy hh:mm:ss" );
+   
+  //  format the current date and time using the defined format
+    String formattedDateTime = now.format(formate);
+
+    //print the formatted date and time
+    System.out.println("Current Date and ");
+
   }
   
 }
