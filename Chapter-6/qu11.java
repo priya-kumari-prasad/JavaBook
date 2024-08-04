690a1a1e73ba26c9fea626d7349e5bc573f4f777
Priya <aadhya.19185@gmail.com>
2024-08-04 06:45:14 +0000

    Aug 4, 2024, 12:15 PM

--- Chapter-6/qu11.java
+++ Chapter-6/qu11.java
@@ -2,8 +2,9 @@ class sameData {
   public static void main(String[] args) {
     String str1 = "Aadhya";
     String str2 = "Aadhya";
-    boolean str = str1.equals(str2);
-    System.out.println(str);
+    if(str1.equals(str2)){
+      System.out.println("Both contains the same data.");
+    }
     
 
   }
