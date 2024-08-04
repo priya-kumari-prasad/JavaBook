584134bc12728f7a3c49f3d9f71f9060db6209cb
Priya <aadhya.19185@gmail.com>
2024-08-04 06:43:44 +0000

    Aug 4, 2024, 12:13 PM

--- Chapter-6/qu11.java
+++ Chapter-6/qu11.java
@@ -1,4 +1,11 @@
 class sameData {
-  
+  public static void main(String[] args) {
+    String str1 = "Aadhya";
+    String str2 = "Aadhya";
+    boolean str = str1.equals(str2);
+    System.out.println(str);
+    
+
+  }
   
 }
