3fada1f608d376feb9b39a1714018597ce9a0006
Priya <aadhya.19185@gmail.com>
2024-08-04 09:05:36 +0000

    Aug 4, 2024, 2:35 PM

--- Chapter-6/qu29.java
+++ Chapter-6/qu29.java
@@ -1,7 +1,8 @@
 class builder {
   public static void main(String[] args) {
-    StringBuilder sbl = new StringBuilder("java is Popular Programming language");
-   sbl.replace(15 , 26, "Famous");
+    StringBuilder sbl = new StringBuilder("java is Popular Programming");
+   sbl.replace(8 , 15, "Famous");
+   sbl.append(" Language");
     System.out.println(sbl);
   }
   
