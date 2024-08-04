cdc3a243d5c5f4c1557b99ecd1c58f0aeb40acbb
Priya <aadhya.19185@gmail.com>
2024-08-04 17:00:49 +0000

    Aug 4, 2024, 10:30 PM

--- Chapter-6/qu30.java
+++ Chapter-6/qu30.java
@@ -1,9 +1,19 @@
+import java.util.StringTokenizer;
+
 class tokenizer {
 public static void main(String[] args) {
   String str = "java is fun and easy to learn";
-  String[] tokens = str.split("\\s+ ");
+  String[] tokens = str.split("\\s+");
   for(String token : tokens){
     System.out.println(token);
+  }
+
+  StringTokenizer tokenizer = new StringTokenizer(str);
+
+  while (tokenizer.hasMoreTokens()) {
+    String token2 = tokenizer.nextToken();
+    System.out.println(token2);
+    
   }
 }  
 }
