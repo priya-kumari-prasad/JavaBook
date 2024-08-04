07350e95ba85c257128f1f83197394f6cf31aec1
Priya <aadhya.19185@gmail.com>
2024-08-04 16:54:59 +0000

    Aug 4, 2024, 10:24 PM

---
+++ Chapter-6/qu30.java
@@ -0,0 +1,9 @@
+class tokenizer {
+public static void main(String[] args) {
+  String str = "java is fun and easy to learn";
+  String[] tokens = str.split("\\s+ ");
+  for(String token : tokens){
+    System.out.println(token);
+  }
+}  
+}
