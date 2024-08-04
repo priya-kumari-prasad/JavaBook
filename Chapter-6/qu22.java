53694cb6d17e54d519d77473f6ccdd41cbdf328c
Priya <aadhya.19185@gmail.com>
2024-08-04 16:25:21 +0000

    Aug 4, 2024, 9:55 PM

--- Chapter-6/qu22.java
+++ Chapter-6/qu22.java
@@ -3,7 +3,7 @@ class replace {
     String str = "ram is the friend of shyam, and he is also friend of anam and jam, all three are best friends.";
     String regex = "am";
    String replaceStr = "tr";
-   System.out.println(replaceStr.replace(regex, replaceStr));
+   System.out.println(str.replaceAll(regex, replaceStr));
     
   }
   
