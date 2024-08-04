9c88664dec2275c62f5a20500a9ded3344d1052f
Priya <aadhya.19185@gmail.com>
2024-08-04 08:54:01 +0000

    Aug 4, 2024, 2:24 PM

--- Chapter-6/qu28.java
+++ Chapter-6/qu28.java
@@ -1,5 +1,10 @@
 class buffer {
   public static void main(String[] args) {
+    StringBuffer sb = new StringBuffer("Java");
+    sb.insert(4, " is Popular ");
+    sb.append("Language");
+    System.out.println(sb.capacity());
+    System.out.println(sb);
     
   }
   
