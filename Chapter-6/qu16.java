5711ea771ffd129c26b6c87c75f7f436ee661916
Priya <aadhya.19185@gmail.com>
2024-08-05 11:29:40 +0000

    Aug 5, 2024, 4:59 PM

---
+++ Chapter-6/qu16.java
@@ -0,0 +1,10 @@
+// wap to get the canonical representation of the string object
+class canonical
+{
+  public static void main(String[] args) {
+    String str1 = new String("Hello, My friend") ;
+    String str2 = new String("Hello, My friend");
+    System.out.println(str1.equals(str2));
+
+  }
+}
