154875a72b38161b5d6e0601f4ee89a32516acc5
Priya <aadhya.19185@gmail.com>
2024-08-05 11:42:39 +0000

    Aug 5, 2024, 5:12 PM

--- Chapter-6/qu09.java
+++ Chapter-6/qu09.java
@@ -2,7 +2,11 @@
 class qu09 {
   public static void main(String[] args) {
     char[] ch = {'h','e','l','l','o'};
+    String[] str =new String[]{};
     System.out.println(ch.length);
+    for(int i = 0;i<ch.length;i++){
+      System.out.println(str[i]);
+    }
 
   }
   
