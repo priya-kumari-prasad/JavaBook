0ba410862a9343438a41ff611e83000f90181bde
Priya <aadhya.19185@gmail.com>
2024-08-05 11:46:57 +0000

    Aug 5, 2024, 5:16 PM

--- Chapter-6/qu09.java
+++ Chapter-6/qu09.java
@@ -1,13 +1,13 @@
-// WAP to crate a new string object with the contents of a character array.
+// WAP to create a new string object with the contents of a character array.
 class qu09 {
   public static void main(String[] args) {
     char[] ch = {'h','e','l','l','o'};
-    String[] str =new String[]{};
+    String[] str ={};
     System.out.println(ch.length);
-    for(int i = 0;i<ch.length;i++){
-      System.out.println(str[i]);
+    for(String str1 : str){
+      System.out.println(str1);
     }
-
+    
   }
   
 }
