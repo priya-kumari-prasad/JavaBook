39b1c015fc1cecb90e9ac205394cb4dc7d0238ac
Priya <aadhya.19185@gmail.com>
2024-08-05 11:36:41 +0000

    Aug 5, 2024, 5:06 PM

--- Chapter-6/qu16.java
+++ Chapter-6/qu16.java
@@ -4,7 +4,15 @@ class canonical
   public static void main(String[] args) {
     String str1 = new String("Hello, My friend") ;
     String str2 = new String("Hello, My friend");
-    System.out.println("Str1 == Str2 :"+str1==str2);
+    System.out.println("Without intern: ");
+    System.out.println("Str1 == Str2 : "+(str1==str2));
+
+    String canoStr1 = str1.intern();
+    String canoStr2 = str2.intern();
+
+
+    System.out.println("\nWith Intern: ");
+    System.out.println("Str1 == Str2 : "+(canoStr1==canoStr2));
 
   }
 }
