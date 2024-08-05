13b0dc19d67f7b38658e26f9c3e563b2b443827b
Priya <aadhya.19185@gmail.com>
2024-08-05 11:39:44 +0000

    Aug 5, 2024, 5:09 PM

---
+++ Chapter-6/qu09.java
@@ -0,0 +1,9 @@
+// WAP to crate a new string object with the contents of a character array.
+class qu09 {
+  public static void main(String[] args) {
+    char[] ch = {'h','e','l','l','o'};
+    System.out.println(ch.length);
+
+  }
+  
+}
