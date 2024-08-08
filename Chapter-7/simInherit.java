1d3703c6a800fb7d285656df5eba70e8212b13f8
Priya <aadhya.19185@gmail.com>
2024-08-08 12:07:07 +0000

    Aug 8, 2024, 5:37 PM

---
+++ Chapter-7/simInherit.java
@@ -0,0 +1,23 @@
+class superclass{
+  String s = "Hello, my friend";
+  public superclass(){
+    System.out.println("Superclass method: "+s);
+
+  }
+}
+class subclass{
+  String i = "Inheritance ";
+  public subclass(){
+    super();
+    System.out.println("Subclass method "+i);
+  }
+}
+
+
+
+public class simInherit {
+  public static void main(String[] args) {
+    subclass sb = new subclass();
+    
+  }
+}
