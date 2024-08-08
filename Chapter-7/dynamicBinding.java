d473ef9d8aaa10379f317cca497ec35656d941c1
Priya <aadhya.19185@gmail.com>
2024-08-08 13:29:59 +0000

    Aug 8, 2024, 6:59 PM

--- Chapter-7/dynamicBinding.java
+++ Chapter-7/dynamicBinding.java
@@ -1,5 +1,30 @@
 // Illustration of run-time polymorphism'
 //superclass
 class shapes{
+  int r = 3 ;
+  double area ;
+  public void calculate(){
+    System.out.println("Area of circle: ");
+  }
+}
+
+class circle extends shapes{
   
+  final double pi = 3.14;
+  public void calculate(){
+  
+    area = pi*r*r;
+    System.out.println("Area of Circle : "+area);
+  }
+}
+
+/**
+ * dynamicBinding
+ */
+public class dynamicBinding {
+
+  public static void main(String[] args) {
+    circle cr = new circle();
+    cr.calculate();
+  }
 }
