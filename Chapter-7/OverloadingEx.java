da261c2cd45dd880199464e5c3510732e5acf489
Priya <aadhya.19185@gmail.com>
2024-08-08 13:12:56 +0000

    Aug 8, 2024, 6:42 PM

---
+++ Chapter-7/OverloadingEx.java
@@ -0,0 +1,38 @@
+//Overloading concept with static binding
+class shapes{
+  int a, r;
+  double area;
+  public void calculate(){
+    System.out.println("Area of circle and square: ");
+  }
+}
+class circle extends shapes{
+  final double pi = 3.14;
+  circle (int r){
+    this.r = r;
+  }
+  public void calculate(int r){
+    area = pi*r*r;
+    System.out.println("Area of circle = "+area);
+  }
+}
+
+class square extends shapes{
+  square (int a){
+    this.a = a;
+    }
+    public void calculate(int a){
+      area = a*a;
+      System.out.println("Area of Square = "+area);
+    }
+}
+
+/**
+ * OverloadingEx
+ */
+public class OverloadingEx {
+
+  public static void main(String[] args) {
+    
+  }
+}
