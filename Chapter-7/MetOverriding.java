5324a59d608282c42fc4cd2c1a5cb2d77e249fa7
Priya <aadhya.19185@gmail.com>
2024-08-08 11:55:00 +0000

    Aug 8, 2024, 5:25 PM

--- Chapter-7/MetOverriding.java
+++ Chapter-7/MetOverriding.java
@@ -1,3 +1,35 @@
 class company{
-  
+  String Sector;
+  String name, headOffice;
+  public void printCompany(){
+    System.out.println("Company Details: ");
+  }
+}
+class branch extends company{
+  String bname, place;
+  public void printCompany(){
+    System.out.println("Company name: "+name);
+    System.out.println("Sector: "+Sector);
+    System.out.println("Head office : "+headOffice);
+
+  }
+  public void printBrance(){
+    System.out.println("Branch name: "+bname);
+    System.out.println("Place Branch: "+place);
+  }
+}
+
+public class MetOverriding {
+
+ public static void main(String[] args) {
+  branch br = new branch();
+  br.name = "Tcs ";
+  br.Sector = "IT";
+  br.headOffice = "Mumbai";
+  br.bname = "Chennai TechPark";
+  br.place = "Chennai";
+  br.printCompany();
+
+  br.printBrance();
+ }
 }
