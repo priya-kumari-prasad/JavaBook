b225f6fa0ee536dcac17daa7c09ba757041bede4
Priya <aadhya.19185@gmail.com>
2024-08-09 17:23:13 +0000

    Aug 9, 2024, 10:53 PM

--- Chapter-7/Hierarchical.java
+++ Chapter-7/Hierarchical.java
@@ -41,9 +41,32 @@ class CreditCard extends payment{
   public String getCreditCardType(){
     return CreditCardType;
   } 
-  public Void setCreditCardType(String CreditCardType){
-    this.CreditCardType = CreditCardType;
+  public Void setCreditCardType(String creditCardType){
+    creditCardType = CreditCardType;
     return null;
-    
   }
+}
+
+public class Hierarchical {
+
+  public static void main(String[] args) {
+    Cheque c1 = new Cheque();
+    c1.setPaymentId(32);
+    c1.setAmount(3400);
+    c1.setChequeType("AXIS");
+    System.out.println("Cheque Id: "+c1.getPaymentId());
+    System.out.println("Cheque Amount: "+c1.getAmount());
+    System.out.println("Cheque Type: "+c1.getChequeType());
+
+
+    CreditCard cc = new CreditCard();
+    cc.setPaymentId(33);
+    cc.setAmount(4300);
+    cc.setCreditCardType("ICICI");
+    System.out.println("Credit Card id: "+cc.getPaymentId());
+    System.out.println("Credit Card Type: "+cc.getCreditCardType());
+    System.out.println("Credit Card Amount: "+cc.getAmount());
+
+  }
+
 }
